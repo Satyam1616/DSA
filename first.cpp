7a1f4765ad9a48c9f0a2e8e773cc5c431054babb
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-09-05 00:59:28 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // cout << "Satyam\n Jha " << endl;
+  // return 0;
+
+  // int age=25;     // Variables - name of variables is identifier.
+  // char grade = 'A';
+  // float PI = 3.14f;
+
+  // bool isSafe = false;
+  // double price = 100.99;
+
+  // cout<< price <<endl;     // true =1, false=0 
+
+  // char grade = 'a'; // Ascii value = 97
+  // int value = grade;
+  // cout<< value << endl;
+
+  // double price;
+  // cout<<"Enter the price: "<< endl;
+  // cin>> price;
+  // cout<< "your price is : "<< price << endl;
+  // return 0;
+
+  // Everything other than cout is boilerplate
+  // Boiler is something which is mandatory to write.
+
+  // Primitive data types 
+  // int -> 4 bytes 8*4=32 bits available to store data.
+  // char -> 1 byte
+  // float -> 4 byte
+  // bool -> 1 byte
+  // double -> 8 byte
+
+  // Type Casting = converting data from one type to another.
+
+  // Operators - Arithmetic= +,*,/,- || Logical
+//   int a = 10, b = 5;
+
+//   cout << "sum =" << (a + b) << endl;
+//   cout << "difference=" << (a - b) << endl;
+//   cout << "product=" << (a * b) << endl;   // Added '=' after "product"
+//   cout << "division=" << (a / b) << endl; // 2 
+//   cout << "modulo=" << (a % b) << endl; // 0
+//   return 0;
+// }
+
+
+// Relational Operator = =, ==, <=, >, >=, !=;
+
+// cout<< (3<5) << endl;
+// cout<< (3>5) << endl;
+// cout<< (3<=5) << endl;
+// cout<< (3!=5) << endl;
+
+// return 0;
+// }
+
+// Logical Operator : OR || AND && NOT !
+
+cout << ( (3<1) || (3<5) ) << endl;
+cout << ( (3<1) && (3<5) ) << endl;
+
+return 0;
+}
