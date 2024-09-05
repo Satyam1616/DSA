d4f8a54a5a683a33ba5f3147567895893da184c2
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-09-06 01:53:55 +0530

    Add files via upload

---
+++ 2conditional.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  // voting system above 18
+
+  // int age;
+  // cout << "ENter your age" << endl;
+  // cin >> age;
+  // if (age > 18)
+  // {
+  //   cout << "you can vote\n";
+  // }
+  // else
+  // {
+  //   cout << "You cannnot vote\n";
+  // }
+
+  // even odd number
+
+  // int n;
+  // cout << "enter value of n\n";
+  // cin >> n;
+  // if (n % 2 == 0)
+  // {
+  //   cout << "Number is even\n";
+  // }
+  // else
+  // {
+  //   cout << "number is odd\n";
+  // }
+
+  // Grading system
+
+  // int marks;
+  // cout << "enter marks\n";
+
+  // cin >> marks;
+
+  // if(marks > 90){
+  //   cout<<"Wow you got A\n";
+  // }
+  // else if(marks >= 80 && marks < 90){
+  //   cout <<" You got B\n";
+  // }
+
+  // else if(marks >= 70 && marks < 80){
+  //   cout <<"You got C\n";
+  // } else{
+  //   cout<<"You got D\n";
+  // }
+
+  // Find character lowercase or uppercase
+
+  // char ch;
+  // cout << "enter character\n";
+  // cin>> ch;
+
+  // if(ch >= 'a' && ch <= 'z'){
+  //   cout<<" character is lower case\n";
+  // }
+  // else if(ch >= 'A' && ch <= 'Z'){
+  //   cout<<" CHARACTER IS UPPERCASE\n";
+  // } else{
+  //   cout<<"Please enter a character\n";
+  // }
+
+  // char ch;
+  // cout << "enter character\n";
+  // cin>> ch;
+
+  // if(ch >= 65 && ch <= 90){
+  //   cout<<" character is uppercase\n";
+  // }
+  // else if(ch >= 97 && ch <= 107){
+  //   cout<<" CHARACTER IS lowercase\n";
+  // } else{
+  //   cout<<"Please enter a character\n";
+  // }
+
+
+    //Ternary Statement
+
+    //condition? stt1: strr2;
+
+      int n=45;
+
+      cout<< (n>=0 ? "positive" : "negative") << endl;
+
+  return 0;
+}
