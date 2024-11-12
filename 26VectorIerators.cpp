4fafce6e75dc690c8f2d003873faa71ed9707abe
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-11-12 23:44:07 +0530

    Add files via upload

--- 26VectorIerators.cpp
+++ 26VectorIerators.cpp
@@ -6,7 +6,21 @@ int main(){
   vector<int> vec = {1,2,3,4,5};
 
   // cout << "vec.begin : "<< *(vec.begin()) << endl;
-  cout << "vec.end : "<< *(vec.end()) << endl;
+  // cout << "vec.end : "<< *(vec.end()) << endl;
+
+  // vector<int>:: iterator it;
+
+  // for(it = vec.begin(); it != vec.end(); it++){
+  //   cout << *(it) <<" ";
+  // }
+
+  // vector<int>:: reverse_iterator it;
+
+  for(auto it = vec.rbegin(); it != vec.rend(); it++){
+    cout << *(it) <<" ";
+  }
+
+  cout << endl;
 
   return 0;
 }
