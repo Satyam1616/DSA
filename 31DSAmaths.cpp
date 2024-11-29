419e782dbb8042619768b771b0e1936d0374025f
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-11-30 01:25:32 +0530

    Add files via upload

--- 31DSAmaths.cpp
+++ 31DSAmaths.cpp
@@ -173,3 +173,21 @@
 
 //L.C.M
 
+#include <iostream>
+using namespace std;
+
+int gcdRec(int a , int b){
+  if(b == 0) return a;
+
+  return gcdRec(b, a % b);
+}
+
+int lcm(int a , int b){
+  int gcd = gcdRec(a,b);
+    return(a*b) / gcd;
+  }
+
+  int main(){
+    cout << lcm(20, 24) << endl;
+    return 0;
+  }
