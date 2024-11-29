408923fbe7248387fe5954082484326b23551e58
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-11-29 23:41:08 +0530

    Add files via upload

--- 31DSAmaths.cpp
+++ 31DSAmaths.cpp
@@ -1,97 +1,128 @@
-// 1.Prime Number
+// // 1.Prime Number
+
+// // // #include <iostream>
+// // // using namespace std;
+
+// // // string isPrime(int n)
+// // // {
+
+// // //   for (int i = 2; i * i <= n; i++)
+// // //   {
+// // //     if (n % i == 0)
+// // //     {
+// // //       return "Non-Prime";
+// // //     }
+// // //     return "Prime";
+// // //   }
+// // // }
+// // //   int main()
+// // //   {
+// // //     int n = 42;
+
+// // //     cout << isPrime(n) << endl;
+// // //   }
+
+
+// // //Sieve of Eratosthenes
 
 // // #include <iostream>
+// // #include <vector>
 // // using namespace std;
 
-// // string isPrime(int n)
-// // {
+// // class Solution {
+// // public:
+// //     int countPrimes(int n) {
+// //         if (n <= 2) return 0; // No primes less than 2
 
-// //   for (int i = 2; i * i <= n; i++)
-// //   {
-// //     if (n % i == 0)
-// //     {
-// //       return "Non-Prime";
+// //         vector<bool> isPrime(n, true);
+// //         int count = 0;
+
+// //         for (int i = 2; i < n; i++) {
+// //             if (isPrime[i]) {
+// //                 count++;
+
+// //                 // Start marking multiples from i*i to avoid redundant operations
+// //                 for (int j = i * i; j < n; j += i) {
+// //                     isPrime[j] = false;
+// //                 }
+// //             }
+// //         }
+
+// //         return count;
 // //     }
-// //     return "Prime";
-// //   }
+// // };
+
+// // int main() {
+// //     Solution sol;
+// //     int n;
+// //     cout << "Enter a number: ";
+// //     cin >> n;
+
+// //     int result = sol.countPrimes(n);
+// //     cout << "Number of primes less than " << n << ": " << result << endl;
+
+// //     return 0;
 // // }
-// //   int main()
-// //   {
-// //     int n = 42;
-
-// //     cout << isPrime(n) << endl;
-// //   }
 
 
-// //Sieve of Eratosthenes
+
+
+// // 2. Digits in a number
 
 // #include <iostream>
-// #include <vector>
 // using namespace std;
 
-// class Solution {
-// public:
-//     int countPrimes(int n) {
-//         if (n <= 2) return 0; // No primes less than 2
+// void printDigits(int n){
+//   int count = 0; //Number of digits
+//   int sum = 0;  //sum of digit
 
-//         vector<bool> isPrime(n, true);
-//         int count = 0;
+//   while (n != 0)
+//   {
+//     int digit = n % 10;
+//     sum+= digit ; //sum of digit
+//     count++; //no. of digits
+//     cout << digit << endl;
 
-//         for (int i = 2; i < n; i++) {
-//             if (isPrime[i]) {
-//                 count++;
+//     n = n/10;
+//   }
+//     cout << count << endl;  // no. of digits
+//     cout << sum << endl;   //sum of digit
+// }
 
-//                 // Start marking multiples from i*i to avoid redundant operations
-//                 for (int j = i * i; j < n; j += i) {
-//                     isPrime[j] = false;
-//                 }
-//             }
-//         }
+// int main(){
+//   int n = 3586;
 
-//         return count;
-//     }
-// };
-
-// int main() {
-//     Solution sol;
-//     int n;
-//     cout << "Enter a number: ";
-//     cin >> n;
-
-//     int result = sol.countPrimes(n);
-//     cout << "Number of primes less than " << n << ": " << result << endl;
-
-//     return 0;
+//   printDigits(n) ;
+//   return 0;
 // }
 
 
+//ArmStrong Number
 
-
-// 2. Digits in a number
 
 #include <iostream>
 using namespace std;
 
-void printDigits(int n){
-  int count = 0; //Number of digits
-  int sum = 0;  //sum of digit
+bool isArmstrong(int n) {
+    int copyN = n;
+    int SumOfCubes = 0;
 
-  while (n != 0)
-  {
-    int digit = n % 10;
-    sum+= digit ; //sum of digit
-    count++; //no. of digits
-    cout << digit << endl;
-
-    n = n/10;
-  }
-    cout << count << endl;  // no. of digits
-    cout << sum << endl;   //sum of digit
+    while (n != 0) {
+        int dig = n % 10;
+        SumOfCubes += (dig * dig * dig);
+        n = n / 10; // Update `n` to avoid an infinite loop
+    }
+  
+    return SumOfCubes == copyN;
 }
 
-int main(){
-  int n = 3586;
+int main() {
+    int n = 111;
 
-  printDigits(n) ;
-  return 0;
+    if (isArmstrong(n)) {
+        cout << n << " is an Armstrong number\n";
+    } else {
+        cout << n << " is not an Armstrong number\n";
+    }
+    return 0;
 }
