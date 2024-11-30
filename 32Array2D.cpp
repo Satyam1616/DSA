22e29805293ef2ab2834b1071f609d62a4bacc70
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-11-30 23:18:29 +0530

    Add files via upload

--- 32Array2D.cpp
+++ 32Array2D.cpp
@@ -1,30 +1,58 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int matrix[4][3];
+//     int rows = 4;
+//     int cols = 3;
+
+//     // Input
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             cin >> matrix[i][j];
+//         }
+//     }
+
+//     // Output
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     // Modifications
+//     cout << matrix[2][1] << endl; // Output original value of matrix[2][1]
+//     matrix[2][1] = 18;            // Modify the value of matrix[2][1]
+//     cout << matrix[2][1] << endl; // Output the updated value of matrix[2][1]
+
+//     return 0;
+// }
+
+
+
+// Linear Search in 2D array
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    int matrix[4][3];
-    int rows = 4;
-    int cols = 3;
-
-    // Input
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cin >> matrix[i][j];
-        }
+bool linearSearch(int mat[][3], int rows, int cols, int key){
+  for(int i=0; i<rows; i++){
+    for(int j = 0; j<cols; j++){
+      if(mat[i][j] == key){
+        return true;
+      }
     }
+  }
+  return false;
 
-    // Output
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+}
 
-    // Modifications
-    cout << matrix[2][1] << endl; // Output original value of matrix[2][1]
-    matrix[2][1] = 18;            // Modify the value of matrix[2][1]
-    cout << matrix[2][1] << endl; // Output the updated value of matrix[2][1]
+int main(){
+  int matrix[4][3] = {{1,2,3}, {4,5,6}, {7,8,9}, {10, 11, 12}};
+  int rows = 4;
+  int cols = 3;
 
-    return 0;
+  cout << linearSearch(matrix, rows, cols, 20) << endl;
+  return 0;
 }
