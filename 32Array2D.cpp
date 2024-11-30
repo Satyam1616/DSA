e3aadd28574c9ae9a5d7e98077f5e57f5b2b5e34
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-11-30 23:59:39 +0530

    Add files via upload

--- 32Array2D.cpp
+++ 32Array2D.cpp
@@ -132,6 +132,7 @@
 
 #include <iostream>
 #include <conio.h>
+#include <vector>
 using namespace std;
 
 int diagonalSum(int mat[][4], int n){
@@ -151,8 +152,21 @@ int diagonalSum(int mat[][4], int n){
 }
 
 int main(){
-  int matrix[4][4]= {{1,2,3,4},{5,6,7,8},{9,10,11,12}, {13,14,15,16}};
-  int n=4;
+  // int matrix[4][4]= {{1,2,3,4},{5,6,7,8},{9,10,11,12}, {13,14,15,16}};
+  // int n=4;
 
-  cout << diagonalSum(matrix, n) << endl;
+  // cout << diagonalSum(matrix, n) << endl;
+
+
+  vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+
+  for(int i =0; i< 3; i++){
+    for(int j=0; j<3;j++){
+      cout << mat[i][j] << endl;
+      
+    }
+  }
+
+  return 0;
+
 }
