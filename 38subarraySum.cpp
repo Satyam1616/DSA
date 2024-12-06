120665395a46e7d6f0ce038412efb0f43f8e6ae2
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2024-12-06 23:12:38 +0530

    Add files via upload

--- 38subarraySum.cpp
+++ 38subarraySum.cpp
@@ -34,3 +34,52 @@
 
 
 // Optimal Approacg
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        int count = 0;
+        vector<int> prefixSum(n, 0);
+
+        // Calculate prefix sums
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        unordered_map<int, int> m;
+        for (int j = 0; j < n; j++) {
+            if (prefixSum[j] == k) count++;
+
+            int val = prefixSum[j] - k;
+            if (m.find(val) != m.end()) {
+                count += m[val];
+            }
+
+            if (m.find(prefixSum[j]) == m.end()) {
+                m[prefixSum[j]] = 0;
+            }
+
+            m[prefixSum[j]]++;
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> arr = {1, 2, 3, 2, 1, 1};
+    int k = 5;
+
+    int result = sol.subarraySum(arr, k);
+    cout << "Number of subarrays: " << result << endl;
+
+    return 0;
+}
