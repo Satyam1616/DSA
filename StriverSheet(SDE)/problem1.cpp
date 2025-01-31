3e0820d45429b8e6e337e1d2a9c47bc9e02d7e5e
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2025-01-31 23:18:11 +0530

    Add files via upload

---
+++ StriverSheet(SDE)/problem1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void markRow(vector<vector<int>> &matrix, int n, int m, int i) {
+    for (int j = 0; j < m; j++) {
+        if (matrix[i][j] != 0) {
+            matrix[i][j] = -1;
+        }
+    }
+}
+
+void markCol(vector<vector<int>> &matrix, int n, int m, int j) {
+    for (int i = 0; i < n; i++) {
+        if (matrix[i][j] != 0) {
+            matrix[i][j] = -1;
+        }
+    }
+}
+
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {  // Fixed `for*`
+            if (matrix[i][j] == 0) {
+                markRow(matrix, n, m, i);
+                markCol(matrix, n, m, j);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {  // Fixed `for(nt j = 0; j < n; j++)`
+            if (matrix[i][j] == -1) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> ans = zeroMatrix(matrix, n, m);  // Fixed `vecor` to `vector`
+
+    cout << "The final matrix is:\n";
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "\n";  // Fixed `cout << n;`
+    }
+
+    return 0;
+}
