368f72377a7a526aff207e0b81ebd687a524efa7
Satyam Jha <145960685+Satyam1616@users.noreply.github.com>
2025-02-18 23:49:31 +0530

    Add files via upload

---
+++ StriverSheet(SDE)/problem23.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+// Function to create a linked list from an array
+ListNode* createLinkedList(int arr[], int n) {
+    if (n == 0) return nullptr;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+
+    for (int i = 1; i < n; i++) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+
+// Function to print the linked list
+void printLinkedList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    ListNode* head = createLinkedList(arr, n);
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    Solution solution;
+    ListNode* middle = solution.middleNode(head);
+
+    cout << "Middle Node Value: " << middle->val << endl;
+
+    return 0;
+}
